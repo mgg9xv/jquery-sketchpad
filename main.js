baf8fc635048a6131596aebd5fe9341f22aae6c1
Michael Gilbertson <mgg9xv@virginia.edu>
2016-01-25 23:18:40 -0500

    Working reset button

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
+
+    setUpSquares();
+
+    $('.square').mouseenter(function(){
+        $(this).addClass('dark');
+    });
+
+    /*
+    $('.square').mouseleave(function(){
+        $(this).removeClass('dark');
+    }); */
+});
+
+function setUpSquares(){
     var pX = 16;
     var pY = 16;
     for ( var i = 0; i < pY; i++) {
         for ( var j = 0; j < pX; j++) {
-            $('.container').append('<div class="square"></div>');
+            $('.grid-container').append('<div class="square"></div>');
         }
     }
-});
+}
+
+function reset(){
+    $('.square').removeClass('dark');
+    var input = prompt("How many pixels per side would you like?");
+}
