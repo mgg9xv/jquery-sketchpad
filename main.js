707775395dc87173be6a9496868df989f8d0def7
Michael Gilbertson <mgg9xv@virginia.edu>
2016-01-25 23:53:26 -0500

    Added prompt and pixel density changing

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-    setUpSquares();
+    setUpSquares(16);
 
-    $('.square').mouseenter(function(){
+    $(document).on('mouseenter', '.square', function(){
         $(this).addClass('dark');
     });
 
@@ -12,17 +12,21 @@ $(document).ready(function(){
     }); */
 });
 
-function setUpSquares(){
-    var pX = 16;
-    var pY = 16;
-    for ( var i = 0; i < pY; i++) {
-        for ( var j = 0; j < pX; j++) {
+function setUpSquares(px){
+    var container_width = 720;
+    var pixel_width = 720 / px;
+    for ( var i = 0; i < px; i++) {
+        for ( var j = 0; j < px; j++) {
             $('.grid-container').append('<div class="square"></div>');
         }
     }
+    $('.square').css('width', pixel_width.toString() );
+    $('.square').css('height', pixel_width.toString() );
 }
 
 function reset(){
     $('.square').removeClass('dark');
     var input = prompt("How many pixels per side would you like?");
+    $('grid-container').empty();
+    setUpSquares(input);
 }
