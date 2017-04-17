561b2f856336f904fbe899878a56efc8bbdb5d61
Michael Gilbertson <mgg9xv@virginia.edu>
2017-04-17 19:54:40 -0400

    Updating the hover styling

---
+++ scripts/grab-screenshots.js
@@ -0,0 +1,8 @@
+const Pageres = require('pageres');
+
+console.log('Started grab-screenshots.js')
+const pageres = new Pageres({delay: 2})
+        .src('mggwxyz.github.io/frontend-nanodegree-arcade-game', ['1280x1024', '1920x1080', '320x568'])
+        .dest(__dirname)
+        .run()
+        .then(() => console.log('Finished grab-screenshots.js'));
